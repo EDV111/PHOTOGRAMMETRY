d3e3f4395d793fb1cb6d8d77d5f62b97bacd0f26
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-13 19:49:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-async function processImages() {
-  const link = document.getElementById("input-link").value;
-  if (!link) {
-    alert("Please enter a valid Google Drive folder link.");
-    return;
-  }
 
-  const url = `https://api.openaerialmap.org/unstable/processing?url=${link}`;
-  const response = await fetch(url);
-  const json = await response.json();
-  const jobId = json.id;
-
-  const intervalId = setInterval(async () => {
-    const jobUrl = `https://api.openaerialmap.org/unstable/processing/${jobId}`;
-   
