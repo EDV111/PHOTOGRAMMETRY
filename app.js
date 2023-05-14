850c5f66a2c071017e48125f9fb7d7c50ca10c81
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-15 00:36:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,110 +1,80 @@
-// global variables
-let camera, scene, renderer, controls, mesh;
-let animationId;
+const canvas = document.querySelector("#canvas");
+const engine = new BABYLON.Engine(canvas, true);
+const scene = new BABYLON.Scene(engine);
+const loader = document.querySelector("#loader-wrapper");
 
-init();
-animate();
-
-function init() {
-  // create camera
-  camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
-    1,
-    1000
-  );
-  camera.position.z = 5;
-
-  // create scene
-  scene = new THREE.Scene();
-
-  // create light
-  const light = new THREE.PointLight(0xffffff, 1);
-  light.position.set(0, 0, 10);
-  scene.add(light);
-
-  // create renderer
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  // create controls
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableZoom = true;
-  controls.enablePan = false;
-
-  // load the default model
-  loadModel("models/teapot.obj");
-
-  // listen for window resize
-  window.addEventListener("resize", onWindowResize);
+function showLoader() {
+  loader.style.display = "block";
 }
 
-function animate() {
-  animationId = requestAnimationFrame(animate);
-
-  // render the scene
-  renderer.render(scene, camera);
+function hideLoader() {
+  loader.style.display = "none";
 }
 
-function loadModel(modelPath) {
-  // cancel the current animation
-  cancelAnimationFrame(animationId);
+function createScene(model) {
+  showLoader();
 
-  // remove any existing mesh
-  if (mesh) {
-    scene.remove(mesh);
-    mesh.geometry.dispose();
-    mesh.material.dispose();
-  }
+  const assetsManager = new BABYLON.AssetsManager(scene);
+  const meshTask = assetsManager.addMeshTask("model task", "", "./", model);
+  meshTask.onSuccess = (task) => {
+    hideLoader();
 
-  // load the new model
-  const loader = new THREE.OBJLoader();
-  loader.load(
-    modelPath,
-    function (object) {
-      // set the new mesh
-      mesh = object;
-      mesh.position.set(0, 0, 0);
-      scene.add(mesh);
+    const modelMesh = task.loadedMeshes[0];
+    modelMesh.position = BABYLON.Vector3.Zero();
 
-      // center the model in the view
-      const box = new THREE.Box3().setFromObject(mesh);
-      const center = box.getCenter(new THREE.Vector3());
-      mesh.position.sub(center);
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      -Math.PI / 2,
+      Math.PI / 2,
+      2,
+      new BABYLON.Vector3(0, 0, 0),
+      scene
+    );
+    camera.attachControl(canvas, true);
 
-      // set the camera position and target
-      const distance = box.getSize(new THREE.Vector3()).length();
-      const direction = controls.target
-        .clone()
-        .sub(camera.position)
-        .normalize();
-      camera.position.copy(direction.multiplyScalar(distance * 1.5)).add(center);
-      controls.target.copy(center);
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
 
-      // start the animation
-      animate();
-    },
-    function (xhr) {
-      // display the loading progress
-      const percentLoaded = (xhr.loaded / xhr.total) * 100;
-      console.log("Model " + modelPath + " " + Math.round(percentLoaded, 2) + "% loaded");
-      updateLoadingBar(percentLoaded);
-    },
-    function (error) {
-      console.error(error);
-    }
-  );
+    scene.createDefaultEnvironment({
+      createGround: false,
+      createSkybox: false,
+    });
+
+    const modelMaterial = new BABYLON.StandardMaterial("model material", scene);
+    modelMesh.material = modelMaterial;
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  };
+
+  assetsManager.onProgress = (remainingCount, totalCount, lastFinishedTask) => {
+    const loadingStatus =
+      (totalCount - remainingCount) / totalCount * 100;
+    console.log(`${loadingStatus}% of assets loaded`);
+
+    const loaderBar = document.querySelector("#loader");
+    loaderBar.style.width = `${loadingStatus}%`;
+  };
+
+  assetsManager.load();
 }
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
+const modelFile = document.querySelector("#modelFile");
+modelFile.addEventListener("change", () => {
+  const file = modelFile.files[0];
+  const reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = () => {
+    const model = JSON.parse(reader.result);
+    createScene(model);
+  };
+});
 
-function updateLoadingBar(percent) {
-  const loadingBar = document.getElementById("loading-bar");
-  loadingBar.style.width = percent + "%";
-}
+window.addEventListener("resize", () => {
+  engine.resize();
+});
 
