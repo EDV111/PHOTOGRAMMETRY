aeea8f31061ab4beb14ea43534b042b5220d5e79
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 03:57:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,110 +1,56 @@
-// Get the file input element
-const fileInput = document.getElementById('file-input');
+// Variables
+let scene, camera, renderer, model;
 
-// Get the progress bar element
-const progressBar = document.getElementById('progress-bar');
+// Initialization
+init();
 
-// Get the start process button
-const startButton = document.getElementById('start-button');
+// Functions
+function init() {
+  // Create a scene
+  scene = new THREE.Scene();
 
-// Get the canvas element
-const canvas = document.getElementById('canvas');
+  // Create a camera
+  const fov = 75;
+  const aspect = window.innerWidth / window.innerHeight;
+  const near = 0.1;
+  const far = 1000;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 5;
 
-// Get the container element
-const container = document.getElementById('container');
+  // Create a renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-// Create a new scene
-const scene = new THREE.Scene();
+  // Create a light
+  const light = new THREE.AmbientLight(0xffffff);
+  scene.add(light);
 
-// Create a new camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // Load the model
+  const loader = new THREE.GLTFLoader();
+  loader.load(
+    // Resource URL
+    'ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb',
+    // onLoad callback
+    function (gltf) {
+      model = gltf.scene;
+      scene.add(model);
+      animate();
+    },
+    // onProgress callback
+    function (xhr) {
+      const loadingPercent = (xhr.loaded / xhr.total) * 100;
+      console.log(`Model loaded: ${loadingPercent}%`);
+    },
+    // onError callback
+    function (error) {
+      console.error('Error loading model:', error);
+    }
+  );
+}
 
-// Create a new renderer
-const renderer = new THREE.WebGLRenderer({ canvas });
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
 
-// Set the size of the renderer
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Add the renderer to the container
-container.appendChild(renderer.domElement);
-
-// Create a new light
-const light = new THREE.AmbientLight(0xffffff);
-
-// Add the light to the scene
-scene.add(light);
-
-// Set the position of the camera
-camera.position.z = 5;
-
-// Create a new loader
-const loader = new THREE.GLTFLoader();
-
-// Create a new variable for the loaded object
-let object;
-
-// Add an event listener to the file input element
-fileInput.addEventListener('change', event => {
-  // Get the file
-  const file = event.target.files[0];
-
-  // Create a new reader
-  const reader = new FileReader();
-
-  // Set the onload function for the reader
-  reader.onload = event => {
-    // Get the result
-    const result = event.target.result;
-
-    // Load the GLTF object
-    loader.load(
-      result,
-      gltf => {
-        // Get the scene from the loaded object
-        object = gltf.scene;
-
-        // Add the object to the scene
-        scene.add(object);
-
-        // Hide the file input element
-        fileInput.style.display = 'none';
-
-        // Hide the start button
-        startButton.style.display = 'none';
-
-        // Show the canvas element
-        canvas.style.display = 'block';
-      },
-      // Add a function to track the loading progress
-      xhr => {
-        const progress = xhr.loaded / xhr.total;
-        progressBar.style.width = progress * 100 + '%';
-      },
-      // Add an error function
-      error => {
-        console.error(error);
-      }
-    );
-  };
-
-  // Read the file
-  reader.readAsDataURL(file);
-});
-
-// Add an event listener to the start button
-startButton.addEventListener('click', () => {
-  // Check if the object is loaded
-  if (object) {
-    // Add the object to the scene
-    scene.add(object);
-
-    // Hide the file input element
-    fileInput.style.display = 'none';
-
-    // Hide the start button
-    startButton.style.display = 'none';
-
-    // Show the canvas element
-    canvas.style.display = 'block';
-  }
-});
