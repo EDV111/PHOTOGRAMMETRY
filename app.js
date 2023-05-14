0abb1bb66523beb003e077d7cac481935fb8d373
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 03:37:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-// DOM elements
-const inputElement = document.getElementById("fileInput");
-const progressBar = document.getElementById("progressBar");
-const viewPortContainer = document.getElementById("viewPortContainer");
+let viewer;
+let loadingBar = document.getElementById('loading-bar');
 
-// GLTF scene
-let scene;
-
-// Load GLTF file on file input change
-inputElement.addEventListener("change", (e) => {
-  const file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  loadGLTF(file);
-});
-
-// Load GLTF function
-function loadGLTF(file) {
-  const reader = new FileReader();
-  reader.readAsArrayBuffer(file);
-  reader.onload = function () {
-    const arrayBuffer = reader.result;
-    const loader = new THREE.GLTFLoader();
-    // Show progress bar
-    progressBar.style.display = "block";
-    loader.load(
-      URL.createObjectURL(new Blob([arrayBuffer])),
-      (gltf) => {
-        scene = gltf.scene;
-        viewPortContainer.appendChild(scene);
-        // Hide progress bar
-        progressBar.style.display = "none";
-      },
-      (xhr) => {
-        // Update progress bar
-        const progress = (xhr.loaded / xhr.total) * 100;
-        progressBar.style.width = `${progress}%`;
-      },
-      (error) => {
-        console.error("Error loading GLTF file:", error);
-      }
-    );
-  };
+function initViewer() {
+  viewer = new Communicator.WebViewer({
+    containerId: 'viewer',
+    endpointUri: '/d2p',
+    model: {
+      filename: 'ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb'
+    },
+    progressBar: {
+      enabled: true,
+      container: loadingBar
+    }
+  });
 }
+
+function startProcess() {
+  const fileInput = document.getElementById('glb-input');
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      const bytes = new Uint8Array(reader.result);
+      viewer.model.load({
+        data: bytes,
+        format: 'glb'
+      });
+    };
+  }
+}
+
+window.onload = () => {
+  const importButton = document.getElementById('import-button');
+  importButton.onclick = () => {
+    const fileInput = document.getElementById('glb-input');
+    fileInput.click();
+  };
+  const fileInput = document.getElementById('glb-input');
+  fileInput.onchange = () => {
+    startProcess();
+  };
+
+  initViewer();
+};
