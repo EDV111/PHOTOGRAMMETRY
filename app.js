49ac285bbae46f57ecd6aaba3398e96a46bedbf6
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 03:15:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
-let gltfLoader = new THREE.GLTFLoader();
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-let fileInput = document.getElementById('file-input');
-fileInput.addEventListener('change', (event) => {
-  let file = event.target.files[0];
-  let reader = new FileReader();
+// Set up the loading manager and progress bar
+const loadingManager = new THREE.LoadingManager();
+const progressBar = document.getElementById("loading-bar");
 
-  reader.addEventListener('load', (event) => {
-    let gltfData = event.target.result;
+loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+  const progress = itemsLoaded / itemsTotal;
+  progressBar.style.width = `${progress * 100}%`;
+};
 
-    gltfLoader.parse(gltfData, '', (gltf) => {
-      let model = gltf.scene;
-      let bbox = new THREE.Box3().setFromObject(model);
-      let center = bbox.getCenter(new THREE.Vector3());
-      let size = bbox.getSize(new THREE.Vector3());
-      let distance = Math.max(size.x, size.y, size.z);
-      let fov = 45;
-      let aspect = window.innerWidth / window.innerHeight;
-      let near = 0.1;
-      let far = distance * 100;
-      let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-      camera.position.set(center.x, center.y, distance * 2.5);
+// Load the GLB model and add it to the scene
+const loader = new THREE.GLTFLoader(loadingManager);
+loader.load('model.glb', (gltf) => {
+  const model = gltf.scene;
+  scene.add(model);
 
-      let controls = new THREE.OrbitControls(camera, document.getElementById('viewport'));
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.05;
-      controls.screenSpacePanning = false;
-      controls.minDistance = distance;
-      controls.maxDistance = distance * 10;
-      controls.maxPolarAngle = Math.PI / 2;
+  // Position the camera to frame the model
+  const box = new THREE.Box3().setFromObject(model);
+  const center = box.getCenter(new THREE.Vector3());
+  const size = box.getSize(new THREE.Vector3());
+  camera.position.set(center.x, center.y, center.z + size.z);
+  camera.lookAt(center);
 
-      let ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-      let directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-      directionalLight.position.set(-1, 1, 1).normalize();
-
-      let scene = new THREE.Scene();
-      scene.add(model);
-      scene.add(ambientLight);
-      scene.add(directionalLight);
-
-      let renderer = new THREE.WebGLRenderer({ antialias: true });
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.getElementById('viewport').appendChild(renderer.domElement);
-
-      function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-        controls.update();
-      }
-
-      animate();
-    });
-  });
-
-  reader.readAsArrayBuffer(file);
+  // Render the scene
+  renderer.render(scene, camera);
 });
-
-
