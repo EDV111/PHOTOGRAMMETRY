9af546bb4512c7730ec7eec65d77b4c932a10170
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 23:50:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,83 +1,95 @@
-// Initialize Cesium Viewer
-var viewer = new Cesium.Viewer('cesiumContainer', {
-  animation: false,
-  timeline: false,
-  sceneModePicker: false,
-  baseLayerPicker: false,
-  geocoder: false,
-  homeButton: false,
-  navigationHelpButton: false,
-  fullscreenButton: false,
-  vrButton: false,
-  selectionIndicator: false,
-  infoBox: false,
-  shouldAnimate: true,
-  skyBox: false,
-  skyAtmosphere: false,
-  contextOptions: {
-    webgl: {
-      alpha: false
-    }
-  }
-});
+$(document).ready(function () {
+  // Display 3D Model
+  function displayModel(file) {
+    var iframe = $('#model-display')[0];
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-// Add a blue box to the scene
-var entity = viewer.entities.add({
-  name: 'Blue box',
-  position: Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
-  box: {
-    dimensions: new Cesium.Cartesian3(400000.0, 300000.0, 500000.0),
-    material: Cesium.Color.BLUE.withAlpha(0.5),
-    outline: true,
-    outlineColor: Cesium.Color.BLACK
-  }
-});
+    iframeDoc.open();
+    iframeDoc.write('<!DOCTYPE html><html><head></head><body></body></html>');
+    iframeDoc.close();
 
-// Hide loading bar and show viewport container when model is ready
-viewer.scene.globe.tileLoadProgressEvent.addEventListener(function () {
-  if (viewer.scene.globe.tilesLoaded === true) {
-    $('#loading-bar-container').hide();
-    $('#viewport-container').show();
-  }
-});
+    var loader = new THREE.GLTFLoader();
+    var camera, scene, renderer;
+    var model;
 
-// Handle file upload
-$('#upload-btn').on('change', function () {
-  var file = this.files[0];
-  var formData = new FormData();
-  formData.append('file', file);
+    function init() {
+      scene = new THREE.Scene();
+      scene.background = new THREE.Color(0x4e4e4e);
 
-  // Show loading bar container
-  $('#loading-bar-container').show();
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+      camera.position.set(0, 0, 3);
 
-  // Upload file
-  $.ajax({
-    url: '/upload',
-    type: 'POST',
-    data: formData,
-    processData: false,
-    contentType: false,
-    xhr: function () {
-      var xhr = $.ajaxSettings.xhr();
-      xhr.upload.onprogress = function (e) {
-        var progress = (e.loaded / e.total) * 100;
-        $('#loading-bar').css('width', progress + '%');
-      };
-      return xhr;
-    },
-    success: function (result) {
-      console.log(result);
+      var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+      scene.add(ambientLight);
 
-      // Load model
-      var tileset = new Cesium.Cesium3DTileset({
-        url: result.url
+      var pointLight = new THREE.PointLight(0xffffff, 0.8);
+      camera.add(pointLight);
+      scene.add(camera);
+
+      var material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      loader.load(file, function (gltf) {
+        model = gltf.scene;
+        model.traverse(function (child) {
+          if (child.isMesh) {
+            child.material = material;
+          }
+        });
+        scene.add(model);
       });
 
-      viewer.scene.primitives.add(tileset);
-
-      // Zoom to model
-      viewer.zoomTo(tileset);
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(window.innerWidth - 50, window.innerHeight - 50);
+      renderer.render(scene, camera);
+      iframeDoc.body.appendChild(renderer.domElement);
     }
+
+    function animate() {
+      requestAnimationFrame(animate);
+      if (model) {
+        model.rotation.y += 0.01;
+      }
+      renderer.render(scene, camera);
+    }
+
+    init();
+    animate();
+  }
+
+  // Upload file
+  $('#upload-form').submit(function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+
+    // Show loading bar
+    $('#loading-bar').addClass('progress');
+
+    $.ajax({
+      url: '/upload',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      xhr: function () {
+        var xhr = $.ajaxSettings.xhr();
+        xhr.upload.onprogress = function (e) {
+          var progress = (e.loaded / e.total) * 100;
+          $('#loading-bar .progress-bar').css('width', progress + '%');
+        };
+        return xhr;
+      },
+      success: function (data) {
+        // Hide loading bar
+        $('#loading-bar').removeClass('progress');
+        $('#loading-bar .progress-bar').css('width', '0%');
+
+        // Display 3D model
+        var fileURL = '/uploads/' + data.filename;
+        displayModel(fileURL);
+      },
+      error: function () {
+        alert('Error uploading file!');
+      },
+    });
   });
 });
 
