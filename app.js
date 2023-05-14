2a0e781aebf0fd1f5e085d7c523b72c6706debe2
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 23:05:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,78 @@
-// DOM elements
-const importBtn = document.querySelector("#import-btn");
-const fileInput = document.querySelector("#file-input");
-const viewer = document.querySelector("#viewer");
+// Select the canvas element
+const canvas = document.querySelector('canvas');
 
-// Loaders
-const gltfLoader = new THREE.GLTFLoader();
-
-// Scene and Camera
+// Initialize the scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  viewer.clientWidth / viewer.clientHeight,
-  0.1,
-  1000
-);
 
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(viewer.clientWidth, viewer.clientHeight);
-viewer.appendChild(renderer.domElement);
+// Initialize the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
 
-// Controls
+// Initialize the renderer
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Create a new instance of GLTFLoader
+const loader = new THREE.GLTFLoader();
+
+// Load the model
+function loadModel(file) {
+  loader.load(
+    file,
+    function (gltf) {
+      // Add the model to the scene
+      scene.add(gltf.scene);
+    },
+    function (xhr) {
+      // Display the loading progress
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+    function (error) {
+      // Display an error message
+      console.error('Failed to load model', error);
+    }
+  );
+}
+
+// Handle the file upload
+const fileUpload = document.getElementById('file-upload');
+fileUpload.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const contents = event.target.result;
+    loadModel(contents);
+  };
+  reader.readAsDataURL(file);
+});
+
+// Initialize the controls
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-// Light
-const light = new THREE.HemisphereLight(0xffffff, 0x444444);
-light.position.set(0, 20, 0);
-scene.add(light);
+// Initialize the clock
+const clock = new THREE.Clock();
 
-// Animate background
-const backgroundCanvas = document.createElement("canvas");
-const backgroundContext = backgroundCanvas.getContext("2d");
-
-backgroundCanvas.width = window.innerWidth;
-backgroundCanvas.height = window.innerHeight;
-
-const backgroundImg = new Image();
-backgroundImg.src = "https://i.imgur.com/7pr8XqT.gif";
-
-backgroundImg.onload = () => {
-  const pattern = backgroundContext.createPattern(backgroundImg, "repeat");
-  backgroundContext.fillStyle = pattern;
-  backgroundContext.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-};
-
-const backgroundTexture = new THREE.CanvasTexture(backgroundCanvas);
-backgroundTexture.wrapS = THREE.RepeatWrapping;
-backgroundTexture.wrapT = THREE.RepeatWrapping;
-backgroundTexture.repeat.set(2, 2);
-
-const backgroundPlaneGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
-const backgroundMaterial = new THREE.MeshBasicMaterial({ map: backgroundTexture, transparent: true });
-const backgroundPlane = new THREE.Mesh(backgroundPlaneGeometry, backgroundMaterial);
-backgroundPlane.rotation.x = -Math.PI / 2;
-backgroundPlane.position.y = -1;
-scene.add(backgroundPlane);
-
-function animateBackground() {
-  backgroundCanvas.width = window.innerWidth;
-  backgroundCanvas.height = window.innerHeight;
-
-  backgroundContext.fillStyle = backgroundMaterial.color.getStyle();
-  backgroundContext.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-
-  backgroundContext.drawImage(backgroundImg, 0, 0, backgroundCanvas.width, backgroundCanvas.height);
-
-  backgroundTexture.needsUpdate = true;
-}
-
-// Resize Renderer on window resize
-window.addEventListener("resize", () => {
-  renderer.setSize(viewer.clientWidth, viewer.clientHeight);
-  camera.aspect = viewer.clientWidth / viewer.clientHeight;
-  camera.updateProjectionMatrix();
+// Create a new instance of AnimationMixer
+let mixer;
+loader.load('animations/model.gltf', function (gltf) {
+  // Load the animations
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  gltf.animations.forEach((clip) => {
+    mixer.clipAction(clip).play();
+  });
+  scene.add(gltf.scene);
 });
 
-// Load model on file input change
-fileInput.addEventListener("change", () => {
-  const file = fileInput.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      const dataURL = reader.result;
-      gltfLoader.load(dataURL, (gltf) => {
-        scene.add(gltf.scene);
-      });
-    };
+// Render the scene
+function render() {
+  requestAnimationFrame(render);
+  const delta = clock.getDelta();
+  if (mixer) {
+    mixer.update(delta);
   }
-});
-
-// Render loop
-function animate() {
-  requestAnimationFrame(animate);
-  animateBackground();
   renderer.render(scene, camera);
-  controls.update();
 }
-animate();
+
+render();
+
 
