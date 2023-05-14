defb81a80e38f81c37ccfc20138861e92da90857
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 03:47:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,110 @@
-let viewer;
-let loadingBar = document.getElementById('loading-bar');
+// Get the file input element
+const fileInput = document.getElementById('file-input');
 
-function initViewer() {
-  viewer = new Communicator.WebViewer({
-    containerId: 'viewer',
-    endpointUri: '/d2p',
-    model: {
-      filename: 'ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb'
-    },
-    progressBar: {
-      enabled: true,
-      container: loadingBar
-    }
-  });
-}
+// Get the progress bar element
+const progressBar = document.getElementById('progress-bar');
 
-function startProcess() {
-  const fileInput = document.getElementById('glb-input');
-  const file = fileInput.files[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onloadend = () => {
-      const bytes = new Uint8Array(reader.result);
-      viewer.model.load({
-        data: bytes,
-        format: 'glb'
-      });
-    };
+// Get the start process button
+const startButton = document.getElementById('start-button');
+
+// Get the canvas element
+const canvas = document.getElementById('canvas');
+
+// Get the container element
+const container = document.getElementById('container');
+
+// Create a new scene
+const scene = new THREE.Scene();
+
+// Create a new camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Create a new renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+// Set the size of the renderer
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Add the renderer to the container
+container.appendChild(renderer.domElement);
+
+// Create a new light
+const light = new THREE.AmbientLight(0xffffff);
+
+// Add the light to the scene
+scene.add(light);
+
+// Set the position of the camera
+camera.position.z = 5;
+
+// Create a new loader
+const loader = new THREE.GLTFLoader();
+
+// Create a new variable for the loaded object
+let object;
+
+// Add an event listener to the file input element
+fileInput.addEventListener('change', event => {
+  // Get the file
+  const file = event.target.files[0];
+
+  // Create a new reader
+  const reader = new FileReader();
+
+  // Set the onload function for the reader
+  reader.onload = event => {
+    // Get the result
+    const result = event.target.result;
+
+    // Load the GLTF object
+    loader.load(
+      result,
+      gltf => {
+        // Get the scene from the loaded object
+        object = gltf.scene;
+
+        // Add the object to the scene
+        scene.add(object);
+
+        // Hide the file input element
+        fileInput.style.display = 'none';
+
+        // Hide the start button
+        startButton.style.display = 'none';
+
+        // Show the canvas element
+        canvas.style.display = 'block';
+      },
+      // Add a function to track the loading progress
+      xhr => {
+        const progress = xhr.loaded / xhr.total;
+        progressBar.style.width = progress * 100 + '%';
+      },
+      // Add an error function
+      error => {
+        console.error(error);
+      }
+    );
+  };
+
+  // Read the file
+  reader.readAsDataURL(file);
+});
+
+// Add an event listener to the start button
+startButton.addEventListener('click', () => {
+  // Check if the object is loaded
+  if (object) {
+    // Add the object to the scene
+    scene.add(object);
+
+    // Hide the file input element
+    fileInput.style.display = 'none';
+
+    // Hide the start button
+    startButton.style.display = 'none';
+
+    // Show the canvas element
+    canvas.style.display = 'block';
   }
-}
-
-window.onload = () => {
-  const importButton = document.getElementById('import-button');
-  importButton.onclick = () => {
-    const fileInput = document.getElementById('glb-input');
-    fileInput.click();
-  };
-  const fileInput = document.getElementById('glb-input');
-  fileInput.onchange = () => {
-    startProcess();
-  };
-
-  initViewer();
-};
+});
