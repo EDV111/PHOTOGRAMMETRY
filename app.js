b7fbf49f83f0351311c33ef8a4b3e77be41e3274
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-15 01:50:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,55 @@
-// Initialize variables
-var scene, camera, renderer;
-var mesh;
+let camera, scene, renderer;
+let model;
 
-// Set up file input element
-var fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('change', loadSTL);
-
-// Create a new scene and set background color
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
-
-// Create a new camera and set position
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a new renderer and set size
-renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Add renderer element to DOM
-document.body.appendChild(renderer.domElement);
-
-// Create a new orbit controls object
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-// Create a new directional light and add to scene
-var light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 0, 1);
-scene.add(light);
-
-// Load STL file
-function loadSTL(event) {
-  var file = event.target.files[0];
-  var loader = new THREE.STLLoader();
-  
-  loader.load(URL.createObjectURL(file), function(geometry) {
-    // Create a new material and mesh from the geometry
-    var material = new THREE.MeshPhongMaterial({ color: 0xff5533, specular: 0x111111, shininess: 200 });
-    mesh = new THREE.Mesh(geometry, material);
-    
-    // Add mesh to scene
-    scene.add(mesh);
-  });
-}
-
-// Render loop
-function animate() {
-  requestAnimationFrame(animate);
-  
-  // Rotate mesh
-  if (mesh) {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-  }
-  
-  // Update controls and render scene
-  controls.update();
-  renderer.render(scene, camera);
-}
-
+init();
 animate();
+
+function init() {
+  const container = document.getElementById('model-container');
+
+  camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 0.1, 1000 );
+  camera.position.set( 0, 0, 2 );
+
+  scene = new THREE.Scene();
+
+  const light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+  light.position.set( 0, 20, 0 );
+  scene.add( light );
+
+  const ambientLight = new THREE.AmbientLight(0xcccccc);
+  scene.add(ambientLight);
+
+  const grid = new THREE.GridHelper(10, 10, 0xffffff, 0xffffff);
+  scene.add(grid);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize( container.clientWidth, container.clientHeight );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  container.appendChild( renderer.domElement );
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+  if (model) {
+    model.rotation.y += 0.005;
+  }
+  renderer.render( scene, camera );
+}
+
+function loadModel() {
+  const file = document.getElementById('file-input').files[0];
+  const reader = new FileReader();
+  reader.addEventListener( 'load', ( event ) => {
+    const contents = event.target.result;
+    const loader = new THREE.GLTFLoader();
+    loader.parse(contents, '', ( gltf ) => {
+      const newModel = gltf.scene;
+      scene.add(newModel);
+      newModel.position.set(0, 0, 0);
+      newModel.scale.set(0.5, 0.5, 0.5);
+      model = newModel;
+    });
+  });
+  reader.readAsArrayBuffer( file );
+}
 
