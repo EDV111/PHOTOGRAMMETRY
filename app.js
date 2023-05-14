6b24dedf486fa2b1d5aa2e514e4fb5d33864ecbb
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 16:54:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-const canvas = document.getElementById("renderCanvas");
+const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
 
-let scene;
-
-const createScene = () => {
-  scene = new BABYLON.Scene(engine);
-
-  const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 2, 5, new BABYLON.Vector3(0, 0, 0), scene);
+// Create a scene
+const createScene = function() {
+  const scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.ArcRotateCamera('camera1', 0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
+  camera.setPosition(new BABYLON.Vector3(0, 5, -10));
   camera.attachControl(canvas, true);
 
-  const light = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(0, 1, 0), scene);
-  light.intensity = 0.7;
+  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
   return scene;
-};
+}
 
-scene = createScene();
+const scene = createScene();
 
-const importButton = document.getElementById("importButton");
-const fileInput = document.getElementById("fileInput");
-
-fileInput.onchange = (event) => {
-  const files = event.target.files;
-  const fileToLoad = files[0];
-
-  const fileReader = new FileReader();
-  fileReader.onload = (event) => {
-    const data = event.target.result;
-
-    BABYLON.SceneLoader.Load("", fileToLoad.name, engine, (scene) => {
-      scene.createDefaultCameraOrLight(true, true, true);
-      scene.activeCamera.alpha += Math.PI;
-      scene.activeCamera.beta += Math.PI;
-
-      scene.activeCamera.attachControl(canvas, true);
-
-      engine.runRenderLoop(() => {
-        scene.render();
-      });
-    });
-  };
-
-  fileReader.readAsArrayBuffer(fileToLoad);
-};
-
-engine.runRenderLoop(() => {
-  scene.render();
+// Resize the engine on window resize
+window.addEventListener('resize', function() {
+  engine.resize();
 });
 
-window.addEventListener("resize", () => {
-  engine.resize();
+// Handle file input change event
+const fileInput = document.getElementById('fileInput');
+fileInput.addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  if (!file) {
+    return;
+  }
+
+  // Load the GLB file
+  const reader = new FileReader();
+  reader.addEventListener('load', function(event) {
+    const arrayBuffer = event.target.result;
+    BABYLON.SceneLoader.ImportMesh('', '', arrayBuffer, scene, function(meshes) {
+      // Remove the previous mesh (if any)
+      scene.meshes.forEach(function(mesh) {
+        mesh.dispose();
+      });
+
+      // Position the mesh at the center of the scene
+      const mesh = meshes[0];
+      mesh.position = new BABYLON.Vector3(0, 0, 0);
+
+      // Resize the mesh to fit the viewport
+      const boundingBox = mesh.getBoundingInfo().boundingBox;
+      const size = boundingBox.maximum.subtract(boundingBox.minimum);
+      const maxSize = Math.max(size.x, size.y, size.z);
+      const scale = 1 / maxSize;
+      mesh.scaling = new BABYLON.Vector3(scale, scale, scale);
+    });
+  });
+  reader.readAsArrayBuffer(file);
 });
 
