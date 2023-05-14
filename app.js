ecb82f2d45fb23d4429f58d24d90e02f332ec733
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 04:12:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,77 +1,57 @@
-// Initialize variables
-let container;
-let camera;
-let scene;
-let renderer;
-let model;
-let loadingBar;
+const canvas = document.querySelector('canvas');
+const progressBar = document.querySelector('.progress-bar');
+const importButton = document.querySelector('#import-button');
 
-init();
+let scene, camera, renderer, model;
 
 function init() {
-  // Create a container for the canvas
-  container = document.createElement("div");
-  container.classList.add("container");
-  document.body.appendChild(container);
-
-  // Create a loading bar
-  loadingBar = new LoadingBar(container);
-
-  // Create a camera
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.25,
-    100
-  );
-  camera.position.set(-5, 3, 10);
-
-  // Create a scene
   scene = new THREE.Scene();
-
-  // Create a renderer and add it to the container
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, 5);
+  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+  renderer.setClearColor("#000000");
   renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
 
-  // Load the GLB file
+  const light = new THREE.AmbientLight(0xffffff);
+  scene.add(light);
+
   const loader = new THREE.GLTFLoader();
   loader.load(
-    "ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb",
-    (gltf) => {
-      // Hide the loading bar
-      loadingBar.hide();
-
-      // Add the model to the scene
+    'ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb',
+    function(gltf) {
       model = gltf.scene;
       scene.add(model);
-
-      // Position the model in the center of the scene
-      const bbox = new THREE.Box3().setFromObject(model);
-      const center = bbox.getCenter(new THREE.Vector3());
-      model.position.sub(center);
-
-      // Render the scene
-      renderer.render(scene, camera);
+      progressBar.style.display = 'none';
     },
-    (progress) => {
-      // Update the loading bar
-      loadingBar.setProgress(progress.loaded / progress.total);
+    function(xhr) {
+      const progress = (xhr.loaded / xhr.total) * 100;
+      progressBar.style.width = `${progress}%`;
     },
-    (error) => {
-      console.error(error);
+    function(error) {
+      console.log('An error happened');
+      console.log(error);
     }
   );
-
-  // Create a resize listener
-  window.addEventListener("resize", onWindowResize);
 }
 
-function onWindowResize() {
-  // Update the camera aspect ratio and renderer size
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+function animate() {
+  requestAnimationFrame(animate);
+  if (model) {
+    model.rotation.x += 0.01;
+    model.rotation.y += 0.01;
+  }
   renderer.render(scene, camera);
 }
+
+importButton.addEventListener('click', () => {
+  init();
+  animate();
+});
+
+window.addEventListener('resize', function() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
 
