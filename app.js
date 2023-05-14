e5dfc56cb920de584df9d3bd9d48aae6037d071e
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-14 04:18:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,37 @@
-const canvas = document.querySelector('canvas');
-const progressBar = document.querySelector('.progress-bar');
-const importButton = document.querySelector('#import-button');
+let modelViewer = document.querySelector('model-viewer');
+let progress = document.querySelector('#progress');
+let importButton = document.querySelector('#import-button');
+let gltfModel;
 
-let scene, camera, renderer, model;
-
-function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 0, 5);
-  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-  renderer.setClearColor("#000000");
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  const light = new THREE.AmbientLight(0xffffff);
-  scene.add(light);
-
-  const loader = new THREE.GLTFLoader();
-  loader.load(
-    'ae1b9eb5e516c46b760028eb98bd7df979687af77ce8fb0117d06f6125ed0f12.glb',
-    function(gltf) {
-      model = gltf.scene;
-      scene.add(model);
-      progressBar.style.display = 'none';
-    },
-    function(xhr) {
-      const progress = (xhr.loaded / xhr.total) * 100;
-      progressBar.style.width = `${progress}%`;
-    },
-    function(error) {
-      console.log('An error happened');
-      console.log(error);
-    }
-  );
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  if (model) {
-    model.rotation.x += 0.01;
-    model.rotation.y += 0.01;
+function showProgress(event) {
+  if (event.lengthComputable) {
+    let percentComplete = event.loaded / event.total * 100;
+    progress.value = percentComplete;
+  } else {
+    console.log('Unable to compute progress as total size is unknown');
   }
-  renderer.render(scene, camera);
 }
 
-importButton.addEventListener('click', () => {
-  init();
-  animate();
-});
+function importGLTF(url) {
+  progress.style.display = 'block';
+  progress.value = 0;
 
-window.addEventListener('resize', function() {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-});
+  // Load the GLTF model
+  gltfModel = modelViewer.model = null;
+  gltfLoader.load(url, function(gltf) {
+    // On load complete
+    gltfModel = modelViewer.model = gltf.scene;
+    progress.style.display = 'none';
+  }, showProgress, function(error) {
+    console.error('Failed to load GLTF model', error);
+    progress.style.display = 'none';
+  });
+}
 
+importButton.addEventListener('change', function() {
+  let file = importButton.files[0];
+  if (file) {
+    importGLTF(URL.createObjectURL(file));
+  }
+});
 
