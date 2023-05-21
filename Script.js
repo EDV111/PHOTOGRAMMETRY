c6a8137415d9127fb89ccc0e58251f086286452c
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-21 23:23:55 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,16 +1,60 @@
-// Random Animation Test Code
-const element = document.querySelector('.checkmark');
-
-// Define a random animation function
-function randomAnimation() {
-  // Add a class to trigger the animation
-  element.classList.add('random-animation');
-
-  // Remove the animation class after a delay
-  setTimeout(() => {
-    element.classList.remove('random-animation');
-  }, 2000); // Adjust the duration of the animation as needed
+body {
+  background-color: black;
+  color: white;
+  font-family: Arial, sans-serif;
 }
 
-// Trigger the random animation on click
-element.addEventListener('click', randomAnimation);
+header, main, footer {
+  padding: 20px;
+}
+
+header {
+  text-align: center;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.slider-container {
+  width: 400px;
+  margin: 0 auto;
+}
+
+.weekly-program {
+  margin-top: 40px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 10px;
+  text-align: left;
+}
+
+th {
+  font-weight: bold;
+}
+
+td input[type="checkbox"] {
+  transform: scale(1.5);
+}
+
+.completed-days {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.day-marker {
+  width: 30px;
+  height: 30px;
+  background-color: white;
+  margin: 0 5px;
+  /* Add animation styles here */
+}
