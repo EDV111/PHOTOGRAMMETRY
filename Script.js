7ee43c6ef0a5645b6a323bec42e78a974a2b94cc
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-21 23:44:32 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,4 +16,26 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  const quotes = [
+    "The only way you gain mental toughness is to do things you're not happy doing.",
+    "If you can see yourself doing something, you can do it. If you can't see yourself doing it, usually you can't achieve it.",
+    "There's a lot of people out there who are so afraid to dream, they've forgotten how to dream.",
+    "The most important conversations you can ever have are the ones you have with yourself.",
+    "Embrace what you are capable of, instead of clinging to what you used to be able to do.",
+    "The biggest disability you can have is a bad mindset.",
+    "Motivation is crap. Motivation comes and goes. When you're driven, whatever is in front of you will get destroyed.",
+    "Suffering is a test. That's all it is. Suffering is the true test of life.",
+    "Life isn't always about doing the things we like to do. It's about doing things we have to do.",
+    "When you think you're done, you're only at 40% of your total potential."
+  ];
+
+  const quoteContainer = document.querySelector('.slider');
+  
+  quotes.forEach(function(quote) {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+    slide.textContent = quote;
+    quoteContainer.appendChild(slide);
+  });
 });
