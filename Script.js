63a98269a7c755885665af0fed36fefe76de932b
EDV111 <132312147+EDV111@users.noreply.github.com>
2023-05-21 23:26:22 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,60 +1,19 @@
-body {
-  background-color: black;
-  color: white;
-  font-family: Arial, sans-serif;
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const dayMarker = document.querySelector('.day-marker');
 
-header, main, footer {
-  padding: 20px;
-}
-
-header {
-  text-align: center;
-}
-
-h1 {
-  font-size: 24px;
-  margin-bottom: 10px;
-}
-
-.slider-container {
-  width: 400px;
-  margin: 0 auto;
-}
-
-.weekly-program {
-  margin-top: 40px;
-}
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 10px;
-  text-align: left;
-}
-
-th {
-  font-weight: bold;
-}
-
-td input[type="checkbox"] {
-  transform: scale(1.5);
-}
-
-.completed-days {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
-}
-
-.day-marker {
-  width: 30px;
-  height: 30px;
-  background-color: white;
-  margin: 0 5px;
-  /* Add animation styles here */
-}
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      if (this.checked) {
+        const marker = document.createElement('div');
+        marker.classList.add('marker-animation');
+        dayMarker.appendChild(marker);
+      } else {
+        const markers = dayMarker.querySelectorAll('.marker-animation');
+        markers.forEach(function(marker) {
+          dayMarker.removeChild(marker);
+        });
+      }
+    });
+  });
+});
